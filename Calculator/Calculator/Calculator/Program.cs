ff9c266dcdb42749e5afd1ff3b90095e1e157b2c
Vadim <167113716+VadimGalstyan@users.noreply.github.com>
2024-09-14 15:56:54 +0400

    Add files via upload

---
+++ Calculator/Calculator/Calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+class Calculator
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Calculator for integers");
+        System.Console.WriteLine("Input operation(+,-,*,/)");
+        string operation = System.Console.ReadLine();
+
+        int a;
+        int b;
+
+        System.Console.WriteLine("input variables in order a,b");
+        int.TryParse(System.Console.ReadLine(), out a);
+        int.TryParse(System.Console.ReadLine(), out b);
+
+        switch (operation)
+        {
+            case "+":
+                System.Console.WriteLine(a + b);
+                break;
+            case "-":
+                System.Console.WriteLine(a - b);
+                break;
+            case "*":
+                System.Console.WriteLine(a * b);
+                break;
+            case "/":
+                System.Console.WriteLine(a / b);
+                break;
+
+            default:
+                System.Console.WriteLine("Invalid operation");
+                break;
+        }
+    }
+}
